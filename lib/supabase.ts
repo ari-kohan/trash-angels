b024d1c96a05cf9e98fe72bcc147c40bcc8e2a0d
Ari Kohan <ari.kohan.1@gmail.com>
2025-03-22 17:21:50 -0700

    no anon key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,7 @@ import 'react-native-url-polyfill/auto';
 
 // Replace with your Supabase URL and anon key
 const supabaseUrl = 'https://ppuvkkldkgfgooafpnkc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwdXZra2xka2dmZ29vYWZwbmtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5NzM4NjgsImV4cCI6MjA1NzU0OTg2OH0.0Uy2TKmECDoS82fsl6qUySLrfjZ86IYjrMpgdQvO-eQ';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
